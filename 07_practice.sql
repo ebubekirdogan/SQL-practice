7bb2454971d08199bd0700eee8191d35b9abf49f
Ebubekir Dogan <ebubekir.dogan@ogr.sakarya.edu.tr>
2025-11-30 22:58:59 +0300

    CONSTRAINTS

--- 07_practice.sql
+++ 07_practice.sql
@@ -64,5 +64,135 @@ ALTER TABLE "Urunler" ALTER COLUMN "uretimSaati" TYPE CHAR(20);
 
 --SEQUENCE
 
+DROP TABLE "sema1"."Urunler";
+
+CREATE TABLE "sema1"."Urunler"(
+"urunNo" SERIAL,
+"kodu" CHAR(6) NOT NULL,
+"adi" VARCHAR(40) NOT NULL,
+"uretimTarihi" DATE DEFAULT '2019-01-01',
+"birimFiyati" MONEY,
+"miktari" SMALLINT DEFAULT 0,
+CONSTRAINT "urunlerPK" PRIMARY KEY("urunNo"),
+CONSTRAINT "urunlerUnique" UNIQUE("kodu"),
+CONSTRAINT "urunlerCheck" CHECK ("miktari" >=0)
+);
+
+INSERT INTO "sema1"."Urunler"
+("kodu","adi","birimFiyati","uretimTarihi","miktari")
+VALUES 
+('ELO004','TV',1300,'2019-10-30',5);
+--urunNo SERIAL  OLARAK TANIMLANDIGI ICIN EKLEME YAPILIRKEN NO GIRILMEDIGI HALDE BENZERSIZ VE OTOMATIK OALRAK ARTAN BIR DEGER ATAR.
+
+
+
+CREATE TABLE "sema1"."Urunler"(
+"urunNo" INTEGER,
+"kodu" CHAR(6) NOT NULL,
+"adi" VARCHAR(40) NOT NULL,
+"uretimTarihi" DATE DEFAULT '2019-01-01',
+"birimFiyati" MONEY,
+"miktari" SMALLINT DEFAULT 0,
+CONSTRAINT "urunlerPK" PRIMARY KEY("urunNo"),
+CONSTRAINT "urunlerUnique" UNIQUE("kodu"),
+CONSTRAINT "urunlerCheck" CHECK ("miktari" >=0)
+);
+
+ALTER TABLE "sema1"."Urunler" ALTER COLUMN "kodu" DROP NOT NULL;
+--urunler tablosundaki kodu sütununun NOT NULL ÖZELLİĞİ KALDIRILDI.
+
+ALTER TABLE "sema1"."Urunler" ALTER "kodu" SET NOT NULL;
+
+INSERT INTO "sema1"."Urunler"
+("kodu","adi","birimFiyati","uretimTarihi","miktari","urunNo")
+VALUES 
+('ELO005','TV',1300,'2019-10-30',5,7);
+
+-- urunNo INTEGER OLARAK TANIMLANDIGINDAN DOLAYI MANUEL EKLEME YAPMAK ZORUNDA KALDIK.
+
+-- TRUNCATE TABLE "sema1"."Urunler"; // TABLO VERİLERİNİ SİLME
+
+
+--DEFAULT
+ALTER TABLE "Urunler" ALTER "uretimTarihi" DROP DEFAULT;
+
+ALTER TABLE "Urunler" ALTER COLUMN "uretimTarihi" SET DEFAULT '2019-01-01';
+
+INSERT INTO "sema1"."Urunler"
+("kodu","adi","birimFiyati","miktari","urunNo")
+VALUES 
+('ELO022','TV',1300,5,21);
+
+--UNIQUE
+ALTER TABLE "sema1"."Urunler" DROP CONSTRAINT "urunlerUnique";
+
+ALTER TABLE "sema1"."Urunler" ADD CONSTRAINT "urunlerUnique" UNIQUE ("kodu");
+
+--İKİ ALANLI UNIQUE ORNEGİ
+
+CREATE TABLE "sema1"."Urunler1"(
+"urunNo" INTEGER,
+"kodu" CHAR(6) NOT NULL,
+"adi" VARCHAR(40) NOT NULL,
+"uretimTarihi" DATE DEFAULT '2019-01-01',
+"birimFiyati" MONEY,
+"miktari" SMALLINT DEFAULT 0,
+CONSTRAINT "urunlerPK" PRIMARY KEY("urunNo"),
+CONSTRAINT "urunlerUnique" UNIQUE("kodu","adi"),
+CONSTRAINT "urunlerCheck" CHECK ("miktari" >=0)
+);
+
+INSERT INTO "sema1"."Urunler1"
+("kodu","adi","birimFiyati","miktari","urunNo")
+VALUES 
+('ELO047','PHONE',1300,5,25);
+
+
+--CHECK
+
+ALTER TABLE "sema1"."Urunler" DROP CONSTRAINT "urunlerCheck";
+
+ALTER TABLE "sema1"."Urunler" ADD CONSTRAINT "urunlerCheck" CHECK("miktari" >=0);
+
+--PRIMARY KEY
+
+ALTER TABLE "sema1"."Urunler" DROP CONSTRAINT "urunlerPK";
+
+ALTER TABLE "sema1"."Urunler" ADD CONSTRAINT "urunlerPK" PRIMARY KEY ("urunNo");
+
+-- İKİ ALANLI PRİMARY KEY 
+
+CREATE TABLE "sema1"."Urunler1"(
+"urunNo" INTEGER,
+"kodu" CHAR(6) NOT NULL,
+"adi" VARCHAR(40) NOT NULL,
+"uretimTarihi" DATE DEFAULT '2019-01-01',
+"birimFiyati" MONEY,
+"miktari" SMALLINT DEFAULT 0,
+CONSTRAINT "urunlerPK2" PRIMARY KEY("adi","kodu"),
+CONSTRAINT "urunlerCheck" CHECK ("miktari" >=0)
+);
+
+INSERT INTO "sema1"."Urunler1"
+("kodu","adi","birimFiyati","miktari","urunNo")
+VALUES 
+('ELO024','PC',1300,5,21);
+
+ALTER TABLE "sema1"."Urunler1" DROP CONSTRAINT "urunlerPK2";
+
+ALTER TABLE "sema1"."Urunler1" ADD CONSTRAINT "urunlerPK2" PRIMARY KEY ("urunNo","adi");
+
+--FOREIGN KEY
+
+
+
+
+
+
+
+
+
+
+
 
 
